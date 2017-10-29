9a76e57e9e48d0fac9339dd38c0b16958babe95c
Smith_Jimenez <tesmith@hotmail.es>
2017-10-28 21:02:59 -0500

    new folder serial de fibonacci

--- serial de fibonacci/fibonacci.php
+++ serial de fibonacci/fibonacci.php
@@ -2,12 +2,22 @@
 	$aux=$_POST['txtnum'];
 	$a=1;
 	$b=1;
-	$c=$a+$b;
-		for($i=1;$i<=$aux;$i++)
+		for($i=0;$i<=$aux;$i++)
 		{
-		echo "<br>".$c;
 		$a=$b;
 		$b=$c;
 		$c=$a+$b;
+
+		// resultado
+		echo "<table border='2'>";
+		echo "<tr>\n";
+		echo "<td>";echo "<br>" .$i;echo "</td>\n";
+		echo "</tr>\n";
+
+			echo "<tr>\n";
+            echo "<td>";echo "<br>" ."$c"."+"."$b"."="."$c\n";
+            echo "</td>\n";
+            echo "</tr>";
+        echo "</table>";
 		}
 ?>
